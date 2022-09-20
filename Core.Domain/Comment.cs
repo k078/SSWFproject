57164c5fd37cd8c191f29dcada525ba0e5ffb9ee
k078 <77967048+k078@users.noreply.github.com>
2022-09-20 13:50:27 +0200

    Domain classes erbij
    Nieuwe classes aan het domain toegevoegd

---
+++ Core.Domain/Comment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Domain
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public DateTime TimeOfComment { get; } //Moet zelf worden vastgesteld door het systeem
+        public Role CommentBy { get; } //Moet zelf worden vastgesteld door eht systeem + Role moet nog employee worden etc
+        public Visibility Visibility { get; set; }
+    }
+}
