e9809edca78d6cc31535f9d4d86c8b2a4c490c37
k078 <77967048+k078@users.noreply.github.com>
2022-09-20 13:18:05 +0200

    Initial commit

---
+++ Core.Domain/Patient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Domain
+{ public class Patient
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName {get{ return this.FirstName + " " + this.LastName; } }
+        public string Email { get; set; } //Email moet uniek zijn, PK
+        public int Telephone { get; set; }
+        public Role Role { get; set; }
+        public int RoleNumber { get; set; }
+        public DateOnly BirthDate { get; set; } //Moet minimaal 16 jaar oud en niet in de toekomst
+        public Gender Gender { get; set; }
+        
+    }
+}
